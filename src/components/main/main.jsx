a03c1032de5231414b704423a9b9c4b7d5e88fc4
Sergey <st@ttsgroup.su>
2020-02-06 23:08:58 +0300

    проблеема

---
+++ src/components/main/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <section><p>привет</p></section>
+  );
+};
+
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+
+// ReactDOM.render(
+//   <App/>,
+//     document.querySelector(`#root`)
+// );
+ReactDOM.render(<h1>Привет</h1>);
