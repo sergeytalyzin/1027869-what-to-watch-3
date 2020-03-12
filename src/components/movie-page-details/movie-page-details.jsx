67b0d01a888a53af8bf31b9edb884b22a87c52c2
Sergey <st@ttsgroup.su>
2020-03-12 21:37:38 +0300

    m4-t2

--- src/components/movie-page-details/movie-page-details.jsx
+++ src/components/movie-page-details/movie-page-details.jsx
@@ -1,46 +1,41 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class MoviePageDetails extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const {genre, date, actors, director, runTime} = this.props.film;
-    return (
-      <div className="movie-card__text movie-card__row">
-        <div className="movie-card__text-col">
-          <p className="movie-card__details-item">
-            <strong className="movie-card__details-name">Director</strong>
-            <span className="movie-card__details-value">{director}</span>
-          </p>
-          <p className="movie-card__details-item">
-            <strong className="movie-card__details-name">Starring</strong>
-            <span className="movie-card__details-value">
-              {actors}
-            </span>
-          </p>
-        </div>
-
-        <div className="movie-card__text-col">
-          <p className="movie-card__details-item">
-            <strong className="movie-card__details-name">Run Time</strong>
-            <span className="movie-card__details-value">{runTime}</span>
-          </p>
-          <p className="movie-card__details-item">
-            <strong className="movie-card__details-name">Genre</strong>
-            <span className="movie-card__details-value">{genre}</span>
-          </p>
-          <p className="movie-card__details-item">
-            <strong className="movie-card__details-name">Released</strong>
-            <span className="movie-card__details-value">{date}</span>
-          </p>
-        </div>
+const MoviePageDetails = (props) => {
+  const {genre, date, actors, director, runTime} = props.film;
+  return (
+    <div className="movie-card__text movie-card__row">
+      <div className="movie-card__text-col">
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Director</strong>
+          <span className="movie-card__details-value">{director}</span>
+        </p>
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Starring</strong>
+          <span className="movie-card__details-value">
+            {actors}
+          </span>
+        </p>
       </div>
-    );
-  }
-}
+
+      <div className="movie-card__text-col">
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Run Time</strong>
+          <span className="movie-card__details-value">{runTime}</span>
+        </p>
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Genre</strong>
+          <span className="movie-card__details-value">{genre}</span>
+        </p>
+        <p className="movie-card__details-item">
+          <strong className="movie-card__details-name">Released</strong>
+          <span className="movie-card__details-value">{date}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
 
 MoviePageDetails.propTypes = {
   film: PropTypes.shape({
--- src/components/movie-page-overview/movie-page-overview.jsx
+++ src/components/movie-page-overview/movie-page-overview.jsx
@@ -1,32 +1,28 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export default class MoviePageOverview extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const MoviePageOverview = (props) => {
 
-  render() {
-    const {rating, ratingCount, ratingLevel, description, actors, director} = this.props.film;
-    return (<React.Fragment>
-      <div className="movie-rating">
-        <div className="movie-rating__score">{rating}</div>
-        <p className="movie-rating__meta">
-          <span className="movie-rating__level">{ratingLevel}</span>
-          <span className="movie-rating__count">{ratingCount} ratings</span>
-        </p>
-      </div>
+  const {rating, ratingCount, ratingLevel, description, actors, director} = props.film;
+  return (<React.Fragment>
+    <div className="movie-rating">
+      <div className="movie-rating__score">{rating}</div>
+      <p className="movie-rating__meta">
+        <span className="movie-rating__level">{ratingLevel}</span>
+        <span className="movie-rating__count">{ratingCount} ratings</span>
+      </p>
+    </div>
 
-      <div className="movie-card__text">
-        {description}
+    <div className="movie-card__text">
+      {description}
 
-        <p className="movie-card__director"><strong>Director: {director}</strong></p>
+      <p className="movie-card__director"><strong>Director: {director}</strong></p>
 
-        <p className="movie-card__starring"><strong>Starring: {actors}</strong></p>
-      </div>
-    </React.Fragment>);
-  }
-}
+      <p className="movie-card__starring"><strong>Starring: {actors}</strong></p>
+    </div>
+  </React.Fragment>);
+};
+
 
 MoviePageOverview.propTypes = {
   film: PropTypes.shape({
@@ -44,3 +40,4 @@ MoviePageOverview.propTypes = {
 };
 
 
+export default MoviePageOverview;
